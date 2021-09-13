ebcd2fb3156343aa98f875a115aa852421d659c4
ashiajoseph <ashiajoseph@gmail.com>
2021-09-14 00:58:29 +0530

    Input elements and related event handlers working. Added footer.

--- app.js
+++ app.js
@@ -18,10 +18,33 @@ const createImgButton = (text,link="") => {
     btn.addEventListener("click",changeButtonStyle)
     return btn
 }
+
+const addColors = (color) => {
+    let opt = document.createElement("option");
+    opt.setAttribute("value",color);
+    opt.textContent = color;
+    bling_color.appendChild(opt);
+}
+
+const addFooterLinks = () => {
+    for(let linkname of Object.keys(imglinks))
+    {  let li= document.createElement("li");
+       let a= document.createElement("a");
+       a.textContent= linkname;
+       a.setAttribute("href","#");
+       a.setAttribute("target","_blank");
+       li.addEventListener("mouseenter",addStyleOnMouseOver);
+       li.addEventListener("mouseleave",removeStyleOnMouseOver);
+       createRelation(li,a,"color: black; text-decoration:none")
+       createRelation(footer_ul,li,"margin-right: 15px;")
+       
+       console.log(linkname)
+    }
+}
+
 // Event Handlers
 const changeText = (e) => { 
     right_h1.textContent= e.target.value;
-    console.log(e.target.value)
 }
 
 const imglinks= { 
@@ -33,14 +56,14 @@ const imglinks= {
 }
 const changeImg = (e) => {
      
-    e.target.style.backgroundColor = e.target.style.backgroundColor === "black" ? "white" : "black";
+    e.target.style.backgroundColor = e.target.style.backgroundColor === "black" ? "transparent" : "black";
 
     let bg = e.target.style.backgroundColor;
 
     if(bg=="black")
     {   e.target.style.color = "white"
         right_div_img.setAttribute("src",imglinks[e.target.name]);
-        right_div_img.style.cssText= "max-width: 30%; padding: 40px 10px"
+        right_div_img.style.cssText= "max-width: 33%; position: absolute; top:59%; z-index: 2"
     }
     else
     {   
@@ -48,9 +71,6 @@ const changeImg = (e) => {
         right_div_img.removeAttribute("src");
         right_div_img.style.cssText= "display: none";   
     }
-    
-    
-    console.log(Object.keys(imglinks))
   
 }
 
@@ -59,11 +79,41 @@ const changeButtonStyle = (e) => {
   for(let btn of allImgButtons )
     {   if(btn.name!= e.target.name)
         {
-            btn.style.backgroundColor = "white";
+            btn.style.backgroundColor = "transparent";
             btn.style.color = "black"; 
         }       
     }
 }
+
+const changeColor = (e) => {
+    document.body.style.backgroundColor= e.target.value;
+}
+
+const changeBlobImg = (e) => {
+    if(e.target.value === "blob")
+    {  blob_img= document.createElement("img");
+       blob_img.setAttribute("src","assets/images/blob.png")
+       blob_img.setAttribute("id","blob")
+       blob_img.style.cssText= "max-width: 49%; position: absolute; top: 30%";
+       createRelation(right_div,blob_img)
+    }
+    else if(e.target.value === "nope" && document.querySelector("#blob")!= "null" )
+        blob_img.remove();    
+}
+
+const addStyleOnMouseOver= (e) => {
+    
+    e.target.style.borderBottom="2px solid";
+    e.target.style.paddingBottom= "5px";
+    console.log("mouse enter")
+}
+
+const removeStyleOnMouseOver= (e) => {
+    
+    e.target.style.borderBottom= "none";
+    console.log("mouse leave")
+}    
+
 
 // Bling Title 
 let title= document.createElement("h3");
@@ -75,7 +125,7 @@ let left= document.createElement("div");
 let right= document.createElement("div");
 
 createRelation(section,left,"flex-basis: 40%;display: flex; flex-direction: column")
-createRelation(section,right,"display: flex; flex-direction: column; flex-basis: 35%; text-align: center")
+createRelation(section,right,"display: flex; flex-direction: column; flex-basis: 35%; text-align: center;position: relative;")
 
 /*  Section - Left */
 let left_h2 = document.createElement("h2")
@@ -109,6 +159,10 @@ bling_name.setAttribute("placeholder","Name your Bling!")
 bling_name.addEventListener("input",changeText)
 
 let bling_color= document.createElement("select");
+let colors= ["Gold","Snow","DodgerBlue","LightSalmon","LightCoral","LightPink","OrangeRed","Tomato","DarkOrchid","SpringGreen","Thistle"]
+for(let color of colors)
+    addColors(color)
+bling_color.addEventListener("change", changeColor)
 
 let bling_radio= document.createElement("div");
 let radio_blob= document.createElement("input");
@@ -118,6 +172,7 @@ radio_blob.setAttribute("value","blob")
 let radio_blob_label= document.createElement("label");
 radio_blob_label.textContent= "Blob";
 
+
 let radio_nope= document.createElement("input"); 
 radio_nope.setAttribute("type","radio");
 radio_nope.setAttribute("name","radio_blob");
@@ -125,41 +180,60 @@ radio_nope.setAttribute("value","nope")
 let radio_nope_label= document.createElement("label");
 radio_nope_label.textContent= "Nope"
 
-createRelation(bling_radio,radio_blob,"")
-createRelation(bling_radio,radio_blob_label,"")
-createRelation(bling_radio,radio_nope,"")
-createRelation(bling_radio,radio_nope_label,"")
+radio_blob.addEventListener("change", changeBlobImg)
+radio_nope.addEventListener("change",changeBlobImg)
 
-createRelation(user_choice,bling_name,"")
-createRelation(user_choice,bling_color,"")
-createRelation(user_choice,bling_radio,"")
+createRelation(bling_radio,radio_blob,"font-size: 0.6em; margin-right: 8px")
+createRelation(bling_radio,radio_blob_label,"margin-right: 10px")
+createRelation(bling_radio,radio_nope,"font-size: 0.6em; margin-right: 8px")
+createRelation(bling_radio,radio_nope_label,"margin-right: 5px")
+
+createRelation(user_choice,bling_name,"border: 1.5px solid black; padding: 5px 10px; border-radius: 5px; background-color: black; margin-right: 12px; color: white; outline: none")
+createRelation(user_choice,bling_color,"border: 1.5px solid black; padding: 5px 10px; border-radius: 5px; background-color: black; margin-right: 12px; color: white; outline: none;")
+createRelation(user_choice,bling_radio,"border: 1.5px solid black; padding: 5px 10px; border-radius: 5px; background-color: black; margin-right: 12px; color: white; outline: none; font-size: 0.8em")
 
 createRelation(left,user_choice,"display: flex")
 
 
 // Section - left - capture
 let capture_div = document.createElement("div");
-let capture_img = document.createElement("img");
-capture_img.setAttribute("src","assets/images/camera.png");
+let capture_inp = document.createElement("input");
+capture_inp.setAttribute("type","image")
+capture_inp.setAttribute("src","assets/images/camera.png")
 
-createRelation(capture_div,capture_img,"")
-createRelation(left,capture_div,"")
+createRelation(capture_div,capture_inp,"max-width: 33px; border: 1.5px solid black; background-color: transparent; padding: 7px; border-radius: 5px")
+createRelation(left,capture_div,"background-color: transparent")
 
 /*  Section - Right */
 let right_h1= document.createElement("h1")
-right_h1.textContent= "Stuff"
+right_h1.textContent= ""
 let right_div= document.createElement("div")
 let right_div_img= document.createElement("img")
 
-
+createRelation(right, right_h1,"font-size: 3em; font-weight: 800 ");
 createRelation(right_div,right_div_img);
-createRelation(right, right_h1,"font-size: 3.5em; font-weight: 800 ");
-createRelation(right,right_div,"display: flex; flex-direction: column; align-items:center; justify-content: center");
+createRelation(right,right_div,"display: flex; flex-direction: column; align-items:center; justify-content: center; ");
+
+/* Dashboard */
+let dashboard = document.createElement("div");
+
+/* Footer */
+let footer = document.createElement("footer");
+let footer_ul = document.createElement("ul");
+//let hearts = document.createElement(&hearts;)
+let li_credits = document.createElement("ul");
+li_credits.textContent= "Credits:"
+//createRelation(footer_ul,hearts)
+createRelation(footer_ul,li_credits,"margin-right: 15px")
+addFooterLinks();
+createRelation(footer,footer_ul,"display:flex; font-weight: bold")
+
+
 
 // Appending to root
 
 createRelation(root,title,"text-align:center; margin-top: 20px; font-weight: 700; font-size: 1.3em")
 createRelation(root,section,"display: flex; justify-content: space-evenly; margin: 50px auto")
-
+createRelation(root,footer,"display: flex; justify-content: center; margin-top: 30px ")
 
 
