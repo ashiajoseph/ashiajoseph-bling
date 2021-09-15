e5ea4ce38d805fff875a98e552ce440cd6aeb888
ashiajoseph <ashiajoseph@gmail.com>
2021-09-16 00:53:14 +0530

    Styling corrected. Minimized use of cssText property

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
 let root = document.querySelector("#root");
-document.body.style.fontFamily= "Montserrat"
-document.body.style.backgroundColor= "gold"
-let bgcolor= "gold";
+document.body.style.fontFamily = "Montserrat";
+document.body.style.backgroundColor = "gold";
+let bgcolor = "gold";
 let selectedImglink = "", blob_visible = false , heading= false;
-let cardsize = "29%"
+let cardsize = "29%";
+
+
 // Functions
 const createRelation = (parent, child, styling= child.style.cssText) => {
     child.style.cssText = styling;
     parent.appendChild(child);
 }
 
-const createImgButton = (text,link="") => {
-    let btn= document.createElement("button");
-    btn.textContent= text;
-    btn.setAttribute('name',text)
-    btn.style.border= "1.5px solid black"
-    btn.style.borderRadius = "5px"
-    btn.style.backgroundColor = "transparent"
-    btn.style.padding= "8px 12px"
-    btn.style.margin= "15px 17px 0px 0px"
-    btn.style.boxShadow= "2px 2px 3px #000"
-    btn.classList.add("bgCheck")
-    btn.addEventListener("click",changeImg)
-    btn.addEventListener("click",changeButtonStyle)
-    btn.addEventListener("mouseover",cursorChange)
-    return btn
+const createImgButton = (text) => {
+    let btn = document.createElement("button");
+    btn.textContent = text;
+    btn.setAttribute('name',text);
+    btn.style.border = "1.5px solid black";
+    btn.style.borderRadius = "5px";
+    btn.style.backgroundColor = "transparent";
+    btn.style.padding = "10px 12px";
+    btn.style.margin = "15px 17px 0px 0px";
+    btn.style.boxShadow = "2px 2px 3px #000";
+    btn.classList.add("bgCheck");
+    btn.addEventListener("click",changeImg);
+    btn.addEventListener("click",changeButtonStyle);
+    btn.addEventListener("mouseover",cursorChange);
+    return btn;
 }
 
 const addColors = (color) => {
@@ -36,15 +38,18 @@ const addColors = (color) => {
 
 const addFooterLinks = () => {
     for(let linkname of Object.keys(imglinks))
-    {  let li= document.createElement("li");
-       let a= document.createElement("a");
-       a.textContent= linkname;
+    {  let li = document.createElement("li");
+       let a = document.createElement("a");
+       a.textContent = linkname;
        a.setAttribute("href","#");
        a.setAttribute("target","_blank");
        li.addEventListener("mouseenter",addStyleOnMouseOver);
        li.addEventListener("mouseleave",removeStyleOnMouseOver);
-       createRelation(li,a,"color: black; text-decoration:none")
-       createRelation(footer_ul,li,"margin-right: 15px; ")
+       createRelation(li,a);
+       a.style.color = "black";
+       a.style.textDecoration = "none";
+       createRelation(footer_ul,li)
+       li.style.marginRight = "15px"
        
     }
 }
@@ -55,14 +60,15 @@ const changeText = (e) => {
     right_h1.style.display = "block"
     right_h1.textContent= e.target.value;
 }
-
+// Image Links - name : link
 const imglinks= { 
   "Polaroid" : "assets/images/polaroid.png",
   "TV" : "assets/images/tv.png",
   "Traitor": "assets/images/among-us.png",
   "Fall Guy": "assets/images/fall-guy-01.png",
   "Radio" : "assets/images/radio-02.png"
-}
+};
+
 const changeImg = (e) => {
     heading=true;
     if(right_h1.style.display=="none") right_h1.style.display= "block";
@@ -71,30 +77,32 @@ const changeImg = (e) => {
     let bg = e.target.style.backgroundColor;
 
     if(bg=="black")
-    {   e.target.style.color = "white"
+    {   e.target.style.color = "white";
         right_div_img.setAttribute("src",imglinks[e.target.name]);
-        right_div_img.style.cssText= "width: 31%; z-index: 2"
-        selectedImglink = imglinks[e.target.name]
+        right_div_img.style.width = "31%";
+        right_div_img.style.zIndex = "2";
+        right_div_img.style.display= "inline-block";
+        selectedImglink = imglinks[e.target.name];
     }
     else
     {   
         e.target.style.color = "black";
         right_div_img.removeAttribute("src");
-        right_div_img.style.cssText= "display: none";   
-        selectedImglink = ""
+        right_div_img.style.display= "none";   
+        selectedImglink = "";
     }
   
 }
 
 const changeButtonStyle = (e) => {
-  let allImgButtons = document.getElementsByClassName("bgCheck")   
+  let allImgButtons = document.getElementsByClassName("bgCheck");  
   for(let btn of allImgButtons )
     {   
         if(btn.name!= e.target.name)
         {
             btn.style.backgroundColor = "transparent";
             btn.style.color = "black"; 
-            btn.style.boxShadow = "2px 2px 3px #000;"
+            btn.style.boxShadow = "2px 2px 3px #000;";
         }       
     }
 }
@@ -111,30 +119,31 @@ const changeBlobImg = (e) => {
     { 
        right_div.style.backgroundImage = "url('assets/images/blob.png')";
        right_div.style.backgroundSize = "50%";
-       right_div.style.backgroundRepeat= "no-repeat"
-       right_div.style.backgroundPosition = "center"
+       right_div.style.backgroundRepeat= "no-repeat";
+       right_div.style.backgroundPosition = "center";
        blob_visible= true;
     }
     else if(e.target.value === "nope" && blob_visible== true )
-       { right_div.style.backgroundImage = "url('')"
-         blob_visible= false;}
+       { right_div.style.backgroundImage = "url('')";
+         blob_visible= false;
+       }
 }
 
 const addStyleOnMouseOver= (e) => {
     
-    e.target.style.borderBottom="2px solid";
-    e.target.style.paddingBottom= "5px";
+    e.target.style.borderBottom = "2px solid";
+    e.target.style.paddingBottom = "5px";
     
 }
 
 const removeStyleOnMouseOver= (e) => {
     
-    e.target.style.borderBottom= "none";
+    e.target.style.borderBottom = "none";
     
 }  
 
 const removeCard = (e) => {
- let card= e.target.parentElement ;
+ let card = e.target.parentElement ;
  card.remove() ;
 }
 
@@ -145,18 +154,40 @@ const createCard = () => {
     card_close.setAttribute("type","image");
     card_close.setAttribute("src","assets/images/icon_delete.png");
     card_close.addEventListener("click",removeCard);
-    let card_h1= document.createElement("h1")
-    if (heading== true) card_h1.textContent= right_h1.textContent;
-    let card_div= document.createElement("div")
-    let card_main_img= document.createElement("img")
-    card_main_img.setAttribute("src",selectedImglink);
-    card_main_img.style.cssText= "width: 46%; z-index: 2; padding: 8px 0px"
+    let card_h1 = document.createElement("h1")
+    if (heading == true) card_h1.textContent= right_h1.textContent;
+    let card_div = document.createElement("div")
 
-    createRelation(card, card_close,"width: 25px; align-self: flex-end; position: relative; top: -10px; left: 10px")
-    createRelation(card, card_h1,"font-size: 3.7em; font-weight: 800; margin: 15px;overflow-wrap: break-word;  ")
+    let card_main_img = document.createElement("img");
+    card_main_img.setAttribute("src",selectedImglink);
+    card_main_img.style.width = "46%";
+    card_main_img.style.zIndex = "2";
+    card_main_img.style.padding = "8px 0px";
+
+    createRelation(card, card_close)
+    card_close.style.width = "37px";
+    card_close.style.alignSelf = "flex-end";
+    card_close.style.position = "relative";
+    card_close.style.top = "-14px";
+    card_close.style.left = "19px";
+
+    createRelation(card, card_h1)
+    card_h1.style.fontSize = "3.7em";
+    card_h1.style.fontWeight = "800";
+    card_h1.style.margin = "15px";
+    card_h1.style.overflowWrap = "break-word";
+
     createRelation(card_div,card_main_img);
 
-    createRelation(card,card_div,"display: flex; flex-direction: column; align-items:center; justify-content: center;  margin: 15px 0px; padding: 40px 10px; min-height: 185px ");
+    createRelation(card,card_div);
+    card_div.style.display = "flex";
+    card_div.style.flexDirection = "column";
+    card_div.style.alignItems = "center";
+    card_div.style.justifyContent = "center";
+    card_div.style.margin = "15px 0px";
+    card_div.style.padding = "40px 10px";
+    card_div.style.minHeight = "185px";
+   
     card.style.cssText=`width: ${cardsize}; border: 2px solid black; display: flex; flex-direction: column;  text-align: center; background-color: ${bgcolor}; margin: 15px 20px ;box-shadow: 2px 2px 3px #000;min-width:270px; min-height: 300px `;
     dashboard.insertBefore(card,dashboard.children[0]);
 
@@ -169,7 +200,7 @@ const createCard = () => {
 
 const displayWhiteBg = (e) => {
     
-    e.target.style.backgroundColor= "white";
+    e.target.style.backgroundColor = "white";
 }
 
 const removeWhiteBg = (e) => {
@@ -177,7 +208,7 @@ const removeWhiteBg = (e) => {
 }
 
 const cursorChange = (e) => {
-    e.target.style.cursor= "pointer"
+    e.target.style.cursor = "pointer";
 }
 
 // Bling Title 
@@ -189,17 +220,35 @@ let section= document.createElement("section");
 let left= document.createElement("div");
 let right= document.createElement("div");
 
-createRelation(section,left,"display: flex; flex-direction: column; margin: 20px 0px 20px 20px")
-createRelation(section,right,"display: flex; flex-direction: column; width: 37%; text-align: center; min-width: 350px;margin: 20px 0px 20px 0px;")
+createRelation(section,left)
+left.style.display = "flex";
+left.style.flexDirection = "column";
+left.style.margin = "20px 0px 20px 20px";
+
+createRelation(section,right)
+right.style.display = "flex";
+right.style.flexDirection = "column";
+right.style.width = "37%";
+right.style.textAlign = "center";
+right.style.minWidth = "350px";
+right.style.margin = "20px 0px 20px 20px";
 
 /*  Section - Left */
-let left_h2 = document.createElement("h2")
-left_h2.textContent = "Sizzle your Life"
-let left_h1 = document.createElement("h1")
-left_h1.textContent = "Make a Card"
+let left_h2 = document.createElement("h2");
+left_h2.textContent = "Sizzle your Life";
+let left_h1 = document.createElement("h1");
+left_h1.textContent = "Make a Card";
 
-createRelation(left,left_h2,"font-size: 1.65em; font-weight: 300; margin: 5px 0px")
-createRelation(left,left_h1,"font-size: 3.0em; font-weight: 700; word-spacing: 0.2em; margin: 5px 0px 15px;")
+createRelation(left,left_h2);
+left_h2.style.fontSize = "1.65em";
+left_h2.style.fontWeight = "300";
+left_h2.style.margin = "5px 0px";
+
+createRelation(left,left_h1)
+left_h1.style.fontSize = "3.0em";
+left_h1.style.fontWeight = "700";
+left_h1.style.margin = "5px 0px 15px";
+left_h1.style.wordSpacing = "0.2em";
 
 // Section - left - image choice
 let img_choice= document.createElement("div");
@@ -214,7 +263,11 @@ createRelation(img_choice,tv);
 createRelation(img_choice,traitor);
 createRelation(img_choice,fallguy);
 createRelation(img_choice,radio);
-createRelation(left,img_choice,"display: flex; margin: 30px 0px 0px ; flex-wrap: wrap;")
+createRelation(left,img_choice)
+
+img_choice.style.display = "flex";
+img_choice.style.margin = "30px 0px 0px";
+img_choice.style.flexWrap = "wrap";
 
 // Section - left - user input
 let user_choice= document.createElement("div");
@@ -248,17 +301,58 @@ radio_nope_label.textContent= "Nope"
 radio_blob.addEventListener("change", changeBlobImg)
 radio_nope.addEventListener("change",changeBlobImg)
 
-createRelation(bling_radio,radio_blob,"font-size: 0.3em; margin: 7px 8px 7px 12px; ")
-createRelation(bling_radio,radio_blob_label,"margin-right: 12px; ")
-createRelation(bling_radio,radio_nope,"font-size: 0.3em; margin: 7px 8px 7px 5px;")
-createRelation(bling_radio,radio_nope_label,"margin-right: 12px; ")
+createRelation(bling_radio,radio_blob)
+radio_blob.style.fontSize = "0.3em";
+radio_blob.style.margin = "7px 8px 7px 12px";
+createRelation(bling_radio,radio_blob_label)
+radio_blob_label.style.marginRight = "12px";
+createRelation(bling_radio,radio_nope)
+radio_nope.style.fontSize = "0.3em";
+radio_nope.style.margin = "7px 8px 7px 5px";
+createRelation(bling_radio,radio_nope_label)
+radio_nope_label.style.marginRight = "12px";
 
-createRelation(user_choice,bling_name,"border: 1.5px solid black; padding: 5px 10px; border-radius: 5px; background-color: black; margin-right: 12px; color: white; outline: none; box-shadow: 2px 2px 3px #000; max-height: 36px; margin-top: 15px ")
-createRelation(user_choice,bling_color,"border: 1.5px solid black; padding: 5px 10px; border-radius: 5px; background-color: black; margin-right: 12px; color: white; outline: none; box-shadow: 2px 2px 3px #000;max-height: 36px; margin-top: 15px ")
-createRelation(user_choice,bling_radio,"border: 1.5px solid black;  border-radius: 5px; background-color: black; margin-right: 5px; color: white; outline: none; font-size: 0.8em; box-shadow: 2px 2px 3px #000;padding: 3px 0px;max-height: 36px; margin-top: 15px  ")
+createRelation(user_choice,bling_name);
+bling_name.style.border = "1.5px solid black";
+bling_name.style.padding = "5px 10px";
+bling_name.style.borderRadius = "5px";
+bling_name.style.backgroundColor = "black";
+bling_name.style.marginRight = "12px";
+bling_name.style.color = "white";
+bling_name.style.boxShadow = "2px 2px 3px #000";
+bling_name.style.maxHeight = "36px";
+bling_name.style.marginTop = "15px";
+bling_name.style.outline = "none";
 
-createRelation(left,user_choice,"display: flex; margin: 15px 0px 25px;flex-wrap: wrap; ")
+createRelation(user_choice,bling_color);
+bling_color.style.border = "1.5px solid black";
+bling_color.style.padding = "5px 10px";
+bling_color.style.borderRadius = "5px";
+bling_color.style.backgroundColor = "black";
+bling_color.style.marginRight = "12px";
+bling_color.style.color = "white";
+bling_color.style.boxShadow = "2px 2px 3px #000";
+bling_color.style.maxHeight = "36px";
+bling_color.style.marginTop = "15px";
+bling_color.style.outline = "none";
 
+createRelation(user_choice,bling_radio);
+bling_radio.style.border = "1.5px solid black";
+bling_radio.style.padding = "3px 0px";
+bling_radio.style.borderRadius = "5px";
+bling_radio.style.backgroundColor = "black";
+bling_radio.style.marginRight = "5px";
+bling_radio.style.color = "white";
+bling_radio.style.fontSize = "0.9em";
+bling_radio.style.boxShadow = "2px 2px 3px #000";
+bling_radio.style.maxHeight = "36px";
+bling_radio.style.marginTop = "15px";
+bling_radio.style.outline = "none";
+
+createRelation(left,user_choice)
+user_choice.style.display = "flex";
+user_choice.style.margin = "15px 0px 25px";
+user_choice.style.flexWrap = "wrap";
 
 // Section - left - capture
 let capture_div = document.createElement("div");
@@ -269,8 +363,16 @@ capture_inp.addEventListener("click",createCard);
 capture_inp.addEventListener("mousedown",displayWhiteBg)
 capture_inp.addEventListener("mouseup",removeWhiteBg)
 
-createRelation(capture_div,capture_inp,"max-width: 37px; border: 1.5px solid black; background-color: transparent; padding: 9px 8px; border-radius: 5px; box-shadow: 2px 2px 3px #000;")
-createRelation(left,capture_div,"background-color: transparent")
+createRelation(capture_div,capture_inp)
+capture_inp.style.maxWidth = "37px";
+capture_inp.style.border = "1.5px solid black";
+capture_inp.style.backgroundColor = "transparent";
+capture_inp.style.padding = "9px 8px";
+capture_inp.style.borderRadius = "5px";
+capture_inp.style.boxShadow = "2px 2px 3px #000;";
+
+createRelation(left,capture_div);
+capture_div.backgroundColor = "transparent";
 
 /*  Section - Right */
 let right_h1= document.createElement("h1")
@@ -278,28 +380,67 @@ right_h1.textContent= "Stuff"
 let right_div= document.createElement("div")
 let right_div_img= document.createElement("img")
 
-createRelation(right, right_h1,"font-size: 3.7em; font-weight: 800 ; display: none; overflow-wrap: break-word;");
-createRelation(right_div,right_div_img,);
-createRelation(right,right_div,"display: flex; flex-direction: column; align-items:center; justify-content: center;  min-height: 240px");
+createRelation(right, right_h1);
+right_h1.style.fontSize = "3.7em";
+right_h1.style.fontWeight = "800";
+right_h1.style.display = "none";
+right_h1.style.overflowWrap = "break-word";
 
+createRelation(right_div,right_div_img);
+createRelation(right,right_div);
+right_div.style.display = "flex";
+right_div.style.flexDirection = "column";
+right_div.style.alignItems = "center";
+right_div.style.justifyContent = "center";
+right_div.style.minHeight = "240px";
 /* Dashboard */
 let dashboard = document.createElement("div");
 dashboard.setAttribute("id","dashboard")
+
 /* Footer */
 let footer = document.createElement("footer");
 let footer_ul = document.createElement("ul");
 
-let li_credits = document.createElement("ul");
+let li_credits = document.createElement("li");
 li_credits.textContent= "🖤 Credits:"
 
-createRelation(footer_ul,li_credits,"margin-right: 15px")
+createRelation(footer_ul,li_credits)
+li_credits.style.marginRight = "10px";
 addFooterLinks();
-createRelation(footer,footer_ul,"display:flex; font-weight: bold; flex-wrap: wrap; margin: 10px 10px; max-width: 80%; justify-content: center")
+
+createRelation(footer,footer_ul)
+footer_ul.style.display = "flex";
+footer_ul.style.fontWeight = "bold";
+footer_ul.style.flexWrap = "wrap";
+footer_ul.style.margin = "10px 10px";
+footer_ul.style.maxWidth = "80%";
+footer_ul.style.justifyContent = "center";
 
 // Appending to root
-createRelation(root,title,"text-align:center; margin: 30px 0px; font-weight: 900; font-size: 1.5em; text-shadow: 3.5px 2.5px 0px #E6A519; letter-spacing: 1px")
-createRelation(root,section,"display: flex; justify-content: space-evenly; margin: 90px 0px 10px; flex-wrap: wrap")
-createRelation(root,dashboard,"display: flex; flex-wrap: wrap; justify-content: center; width: 85%; margin: auto; ")
-createRelation(root,footer,"display: flex; justify-content: center; margin: 30px 0px 10px ; padding: 10px 0px; text-align: center")
+createRelation(root,title);
+title.style.textAlign = "center";
+title.style.textShadow = "3.5px 2.5px 0px #E6A519";
+title.style.fontWeight = "900";
+title.style.fontSize = "1.5em";
+title.style.letterSpacing = "1px";
+title.style.margin = "30px 0px";
 
+createRelation(root,section)
+section.style.display = "flex";
+section.style.justifyContent = "space-evenly";
+section.style.flexWrap = "wrap";
+section.style.margin = "90px 0px 10px";
+
+createRelation(root,dashboard);
+dashboard.style.display = "flex";
+dashboard.style.justifyContent = "center";
+dashboard.style.flexWrap = "wrap";
+dashboard.style.width = "85%"
+dashboard.style.margin = "auto";
+
+createRelation(root,footer)
+footer.style.display = "flex";
+footer.style.justifyContent = "center";
+footer.style.margin = "50px 0px 10px";
+footer.style.padding = "10px 0px";
 
